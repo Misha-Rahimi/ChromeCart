30f0da596cc7241b85a97157023974d29a9c08cd
Misha-Rahimi <mlrahimi@purdue.edu>
2021-01-23 14:00:02 -0500

    Extracts quantity from amazon and price from ebay

--- testInject.js
+++ testInject.js
@@ -2,7 +2,16 @@ var button = document.getElementById("add-to-cart-button");
 if (button !== null) {
 	button.addEventListener("click", function() {
 		var price = document.getElementById("price_inside_buybox").innerText;
-		alert("You added an item to your cart that costs " + price);
+		var quantity = document.getElementsByClassName("a-dropdown-prompt")[0].innerText;
+		alert("You added " + quantity + " items that cost " + price + " each");
+	}, false);
+}
+
+var ebayButton = document.getElementById("isCartBtn_btn");
+if (ebayButton !== null) {
+	ebayButton.addEventListener("click", function() {
+		var price = document.getElementById("prcIsum").innerText;
+		alert("ebay add to cart" + price);
 	}, false);
 }
 
