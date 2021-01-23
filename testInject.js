60a6d7579622bdb43bbc01f3d860096a8ee5298c
Misha-Rahimi <mlrahimi@purdue.edu>
2021-01-23 13:06:16 -0500

    Alert when add to cart clicked on Amazon

---
+++ testInject.js
@@ -0,0 +1,7 @@
+var button = document.getElementById("add-to-cart-button");
+if (button !== null) {
+	button.addEventListener("click", function() {
+		alert("add to cart clicked");
+	}, false);
+}
+
