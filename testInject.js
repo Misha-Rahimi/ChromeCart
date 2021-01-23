ecaf80e9f661d93b213d16a894b16fda397dd196
Misha-Rahimi <mlrahimi@purdue.edu>
2021-01-23 15:11:03 -0500

    commit

--- testInject.js
+++ testInject.js
@@ -3,7 +3,7 @@ if (button !== null) {
 	button.addEventListener("click", function() {
 		var price = document.getElementById("price_inside_buybox").innerText;
 		var quantity = document.getElementsByClassName("a-dropdown-prompt")[0].innerText;
-		alert("You added " + quantity + " items that cost " + price + " each");
+		alert("You added " + quantity + " items that cost " + price + " each to your ChromeCart");
 	}, false);
 }
 
@@ -11,7 +11,7 @@ var ebayButton = document.getElementById("isCartBtn_btn");
 if (ebayButton !== null) {
 	ebayButton.addEventListener("click", function() {
 		var price = document.getElementById("prcIsum").innerText;
-		alert("ebay add to cart" + price);
+		alert("ebay add to ChromeCart" + price);
 	}, false);
 }
 
