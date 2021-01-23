a395fbf34d954fbf2416df74185eea154b12db06
Misha-Rahimi <mlrahimi@purdue.edu>
2021-01-23 13:38:17 -0500

    Extracts item price from Amazon

--- testInject.js
+++ testInject.js
@@ -1,7 +1,9 @@
 var button = document.getElementById("add-to-cart-button");
 if (button !== null) {
 	button.addEventListener("click", function() {
-		alert("add to cart clicked");
+		var price = document.getElementById("price_inside_buybox").innerText;
+		alert("You added an item to your cart that costs " + price);
 	}, false);
 }
+
 
