0be1733e2147345bce3a40166796287251e5c31a
johanan-daniel <70776118+johanan-daniel@users.noreply.github.com>
2021-01-24 01:16:39 -0500

    Added submit button

--- popup.js
+++ popup.js
@@ -25,63 +25,12 @@
 	} //makeList()
 	document.getElementById("makeList").onclick = makeList;
 
-		function edit() {
-        console.log("peepeepoopoo");
-        var elements = document.getElementById("delete");
-
-        for (var i = 0, element; (element = elements[i++]); ) {
-          console.log(element);
-        }        
-		if (document.getElementById("editButton").innerHTML === "Edit cart") {
-          document.getElementById("editButton").innerHTML = "Delete Selected Items";
-          console.log(document.getElementById("editButton").innerHTML);
-
-          for (var i = 0, element; (element = elements[i++]); ) {
-            element.disabled = false;
-          }
-        } else {
-          document.getElementById("editButton").innerHTML = "Edit cart";
-          console.log(document.getElementById("editButton").innerHTML);
-
-          for (var i = 0, element; (element = elements[i++]); ) {
-            element.disabled = true;
-          }
-          elements = deleter();
-        }
-      } //edit()
-		document.getElementById("editButton").onclick = edit;
-
-      function deleter() {
-        var inputs = document.getElementsByTagName("input");
-        var labels = document.getElementsByTagName("label");
-        console.log(inputs.length);
-        for (var i = 0; i < inputs.length; i++) {
-          var inp = inputs[i];
-          var l = labels[i];
-          console.log(inp.checked);
-          if (inp.checked == true) {
-            inputs[i].parentNode.removeChild(inputs[i]);
-            labels[i].parentNode.removeChild(lables[i]);
-          }
-        }
-      }
-
+	  //spam opens all carts
       document.getElementById("submit").onclick = function () {
-      //  var checkbox = document.createElement("input");
-      //  checkbox.type = "checkbox";
-      //  checkbox.id = "car";
-      //  checkbox.name = "interest";
-      //  checkbox.value = "car";
+			window.open("https://cart.ebay.com/");
+			window.open("https://www.walmart.com/cart");
+			window.open("https://www.amazon.com/gp/cart/view.html?ref_=nav_cart");
+			window.open("https://www.macys.com/my-bag?cm_sp=navigation-_-top_nav-_-bag&lid=glbtopnav_bag-us");
 
-      //  var label = document.createElement("label");
-      //  label.htmlFor = "car";
-      //  label.appendChild(document.createTextNode("Car"));
-
-      //  var br = document.createElement("br");
-
-      //  var container = document.getElementById("container");
-      //  container.appendChild(checkbox);
-      //  container.appendChild(label);
-      //  container.appendChild(br);
       };
 
